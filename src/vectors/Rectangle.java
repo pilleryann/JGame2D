dac29367ec70d8772ded2a4d37863fadd1958ce0
yann <yann@yann-ThinkPad-W510>
2012-04-04 14:26:23 +0200

    Add Rectangle class

---
+++ src/vectors/Rectangle.java
@@ -0,0 +1,33 @@
+package vectors;
+
+public class Rectangle implements Move,Scale,Rotate{
+     
+     public Rectangle(Point point2D,float width,float height){
+          
+     }
+
+     @Override
+     public void rotate(float radian) {
+          // TODO Auto-generated method stub
+          
+     }
+
+     @Override
+     public void scale(float scalar) {
+          // TODO Auto-generated method stub
+          
+     }
+
+     @Override
+     public void setLocation(Point p) {
+          // TODO Auto-generated method stub
+          
+     }
+
+     @Override
+     public void move(Vector2D v) {
+          // TODO Auto-generated method stub
+          
+     }
+
+}
